c68603ee07b1579ae579b1c61d65d636f3e4f9f3
iqeq00 <iqeq00@gmail.com>
2019-05-10 17:20:21 +0800

    First commit

---
+++ src/main/java/com/lichee/microservices/MicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.lichee.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesApplication.class, args);
+	}
+
+}
